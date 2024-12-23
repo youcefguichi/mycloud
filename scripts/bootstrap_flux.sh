57f3640904d8ebdc86a88a61087044767d79365b
Youcef Guichi <youcefelachir@gmail.com>
2024-12-23 16:01:57 +0100

    create a structure for k8s infra

--- scripts/bootstrap_flux.sh
+++ scripts/bootstrap_flux.sh
@@ -1,5 +1,6 @@
+
 kubectx admin@kafka-failover
 flux bootstrap github --owner=youcefguichi --repository=mycloud --path=gitops-k8s-infra/failover/flux
 
-kubectx admin@kafka-failover
+kubectx admin@kafka-production
 flux bootstrap github --owner=youcefguichi --repository=mycloud --path=gitops-k8s-infra/prod/flux
---
+++ scripts/start_clusters.sh
@@ -0,0 +1,6 @@
+
+# Start kafka nodes for both prod an failover clusters
+docker start c9867e70332a
+docker start ef3b2f75bc2e
+docker start 3c668a68a2ee
+docker start 780d77f54cee
