ec01ec4c20efb2cb15ad5f16b1e00f114d6dd8b1
Youcef Guichi <youcefelachir@gmail.com>
2024-12-25 20:10:33 +0100

    downscale replicas to 1

--- scripts/bootstrap_flux.sh
+++ scripts/bootstrap_flux.sh
@@ -1,6 +1,6 @@
 
-kubectx admin@kafka-failover
+kubectx k3d-kafka-failover
 flux bootstrap github --owner=youcefguichi --repository=mycloud --path=gitops-k8s-infra/failover/flux
 
-kubectx admin@kafka-production
+kubectx k3d-kafka-production
 flux bootstrap github --owner=youcefguichi --repository=mycloud --path=gitops-k8s-infra/prod/flux
--- scripts/start_clusters.sh
+++ scripts/start_clusters.sh
@@ -1,6 +1,6 @@
 
 # Start kafka nodes for both prod an failover clusters
-docker start c9867e70332a
-docker start ef3b2f75bc2e
-docker start 3c668a68a2ee
-docker start 780d77f54cee
+docker stop c9867e70332a
+docker stop ef3b2f75bc2e
+docker stop 3c668a68a2ee
+docker stop 780d77f54cee
